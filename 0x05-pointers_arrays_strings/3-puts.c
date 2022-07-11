d67fedad790a97e2e738c6c62362d6c45d0d34cb
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-07-11 16:25:35 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * _puts - prints a string to stdout
- * @str: 
+ * @str: pointer variable
  * Return: void
  */
 void _puts(char *str)
 {
 	int i = 0;
-	
+
 	while (*(str + i) != '\0')
 	{
 		_putchar(str[i]);
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,7 @@ void print_rev(char *s)
 
 	while (s[i])
 		i++;
-	while(i--)
+	while (i--)
 		_putchar(s[i]);
 	_putchar('\n');
 
