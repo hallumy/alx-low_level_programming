b22a112697408b7035102836a155b4c85bc61d0d
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-07-20 16:46:52 +0300

    Dreams feel real while we are in themwq

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string in use
+ * Return: Returns the length of a string
+ * Otherwise returns NULL
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
