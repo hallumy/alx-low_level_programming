2b7a49c72cd481f8a1975acf8dcaed2138d15422
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-07-27 19:16:27 +0300

    Corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	return (NULL);
 	
-	str = malloc(size(char) * size);
+	str = malloc(sizeof(char) * size);
 	if (str == NULL)
 		return (NULL);
 	
