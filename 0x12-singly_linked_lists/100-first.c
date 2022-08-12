1041836ad6a742fcc83e283acbed445eb81c7d8a
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-08-13 01:20:24 +0300

    A function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void hare_and_tortoise(void)__attribute__ ((constructor));
+
+/**
+ * hare_and_tortoise - Prints a string before the main 
+ * 			function is executed
+ */
+
+void hare_and_tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,4 +25,5 @@ int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void hare_and_tortoise(void);
 #endif
