b2dc14a8d0006d0bc0ab5c7f7e3a24946ae9d7eb
Ahlam Abdulkhalek <alhamny@gmail.com>
2022-08-13 00:55:46 +0300

    Added to free the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list- Frees a list_t list
+ * @head: Pointer to the list_t list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	free(head);
+}
